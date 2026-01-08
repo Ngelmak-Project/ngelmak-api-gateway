083427c8b2f3a6441828250d43667fb9aef3e5b8
Youssouph Faye <youssouph.faye@gmail.com>
2026-01-08 16:04:55 +0100

    Update configuration for CORS

--- src/main/java/org/ngelmakproject/NgelmakApiGatewayApplication.java
+++ src/main/java/org/ngelmakproject/NgelmakApiGatewayApplication.java
@@ -2,29 +2,12 @@ package org.ngelmakproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class NgelmakApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NgelmakApiGatewayApplication.class, args);
-	}
-
-	@Bean
-	public CorsWebFilter corsWebFilter() {
-		CorsConfiguration config = new CorsConfiguration();
-		config.addAllowedOrigin("*");
-		config.addAllowedMethod("*");
-		config.addAllowedHeader("*");
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);
-
-		return new CorsWebFilter(source);
 	}
 
 }
